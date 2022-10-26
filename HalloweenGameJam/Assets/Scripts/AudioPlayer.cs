d79bdfd3acbf2397410ccb3baf5c02a12e0a970b
CiaraDonaldson <94332296+CiaraDonaldson@users.noreply.github.com>
2022-10-26 17:18:25 -0400

    some rain

--- HalloweenGameJam/Assets/Scripts/AudioPlayer.cs
+++ HalloweenGameJam/Assets/Scripts/AudioPlayer.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class AudioPlayer : MonoBehaviour
 {
     public AudioSource audioSource;
-    public float delay = 4;
+    public float delay;
+
 
     void Start()
     {
+
         audioSource.PlayDelayed(delay);
     }
 }
